517d9df7952028e864be112c8d1fad2d4304802f
Giovanni Miranda <giovannimiranda09@gmail.com>
2026-06-22 16:06:55 -0400

    update

--- app/components/AILightEffect.tsx
+++ app/components/AILightEffect.tsx
@@ -2,11 +2,11 @@ export default function AILightEffect() {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none fixed inset-0 -z-10 overflow-hidden"
+      className="fixed inset-0 -z-10 overflow-hidden pointer-events-none"
     >
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(56,189,248,0.08),transparent_65%)]" />
-      <div className="absolute top-1/4 -left-40 h-[32rem] w-[32rem] rounded-full bg-sky-500/15 blur-[140px]" />
-      <div className="absolute -right-32 -bottom-48 h-[36rem] w-[36rem] rounded-full bg-blue-500/20 blur-[150px]" />
+      <div className="absolute -left-40 top-1/4 h-128 w-lg rounded-full bg-sky-500/15 blur-[140px]" />
+      <div className="absolute -bottom-48 -right-32 h-144 w-xl rounded-full bg-blue-500/20 blur-[150px]" />
     </div>
-  )
+  );
 }
